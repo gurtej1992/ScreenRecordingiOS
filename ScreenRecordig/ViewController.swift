f46962a9a9980174e95fd04a1181c0881828567d
Tej <codewithtej@gmail.com>
2021-05-25 12:17:17 -0400

    Finish

--- ScreenRecordig/ViewController.swift
+++ ScreenRecordig/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import AVKit
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imagview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(fileURLWithPath: Bundle.main.path(forResource: "video", ofType: "mp4")!)
+        imagview.contentMode = .scaleAspectFill
+        imagview.image = url.generateThumbnail()
+        
+    }}
+extension URL {
+    func generateThumbnail() -> UIImage? {
+        do {
+            let asset = AVURLAsset(url: self)
+            let imageGenerator = AVAssetImageGenerator(asset: asset)
+            imageGenerator.appliesPreferredTrackTransform = true
+            let cgImage = try imageGenerator.copyCGImage(at: CMTime(seconds: 5, preferredTimescale: 60),
+                                                         actualTime: nil)
+            return UIImage(cgImage: cgImage)
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
     }
-
-
 }
-
